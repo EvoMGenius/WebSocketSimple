49d2bc3cc2f741a366b1edb391f7e6a23d851b3e
EvoMGenius <apatrios@mail.ru>
2022-01-20 20:18:58 +1000

    done simple web-socket app

---
+++ src/test/java/com/apatrios/WebSocketChat/WebSocketChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apatrios.WebSocketChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
